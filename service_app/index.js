f9f841a3ee51c891b0482d646af9e4d59d862d39
Arky Asmal <arkyasmal@gmail.com>
2022-09-09 21:58:50 -0400

    "migrated electron app to electron forge to package the app"

--- service_app/index.js
+++ service_app/index.js
@@ -1,16 +1,20 @@
 const express = require("express");
 const cors = require("cors");
 const path = require("path");
+const dotenv = require("dotenv");
+const fs = require("fs");
 const createLocalTunnel = require(path.join(__dirname, "generateLocalTunnel"));
 const postRoute = require(path.join(__dirname, "routes/post"));
-const dotenv = require("dotenv");
 const configPath = path.join(__dirname, "..", "config.env");
 const app = express();
+fs.writeFileSync(`${__dirname}/server.txt`, "hello");
 const startServer = async () => {
   //parses environment variables
+  fs.writeFileSync(`${__dirname}/server.txt`, "hello app");
   dotenv.config({ path: configPath });
   //create local tunnel
   await createLocalTunnel();
+
   //support all orgins
   const corsOptions = {
     origin: ["*"],
@@ -36,3 +40,4 @@ if (typeof require !== "undefined" && require.main === module) {
   startServer();
 }
 module.exports = startServer;
+//    //"build:gui": "npm exec electron-forge . --executable-name webhook_pc_power_controls --electron-version 20.1.2 --no-prune --arch x64"
--- service_app/service_files/serviceInstall.js
+++ service_app/service_files/serviceInstall.js
@@ -1,22 +1,30 @@
 const path = require("path");
 const Service = require("node-windows").Service;
+const fs = require("fs");
 const serviceInstall = (callback = null) => {
   const serviceName = "Control PC Power Settings";
-  const scriptPath = path.join(__dirname, "..", "index.js");
+  const scriptPath = path.join(__dirname, "..", "index.js").replaceAll("\\", "\\\\");
+  const workingDirectory = path.join(__dirname, "..", "..");
+  //const execPath = path.join(__dirname, "..", "..", "node_modules/electron/dist/electron.exe");
   const svc = new Service({
     name: serviceName,
+    //execPath: execPath, 
     description:
       "Control sleep, logout or shutdown with using a web server to respond to webhooks",
     script: scriptPath,
     maxRestarts: 1,
+    //workingDirectory: workingDirectory,
   });
 
   svc.on("install", function () {
     svc.start();
+    fs.writeFileSync(
+      `${__dirname}/service_install.txt`,
+      `${scriptPath}, ${workingDirectory}`
+    );
     console.log("App install complete");
     if (callback) callback();
   });
-
   svc.install();
 };
 //run script through node
