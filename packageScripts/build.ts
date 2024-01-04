3cf0a7527a9dfb664e3d82935ba94079c0895b76
Arky Asmal <arkyasmal@gmail.com>
2024-01-04 01:56:26 -0500

    added the generation of node.exe to build script

--- packageScripts/build.ts
+++ packageScripts/build.ts
@@ -1,11 +1,28 @@
 import { execCommand } from "./utils/execCommand";
 import path from "path";
-import { copyAndReplaceFiles } from "./utils/copyAndReplaceFiles.";
+import {
+  copyAndReplaceFiles,
+  copyAndReplaceFile,
+} from "./utils/copyAndReplaceFiles.";
+import { determineExecPath } from "../src/service_app/utils/downloadNode";
 const nodeModulesPath = path.join(__dirname, "../node_modules", ".bin", "tsc");
 const publicFilesSrc = path.join(__dirname, "../src/service_gui/public");
 const publicFilesDest = path.join(__dirname, "../build/src/service_gui/public");
 export const buildScript = async () => {
   await execCommand(`"${nodeModulesPath}"`);
+  const nodeExecPath = await determineExecPath();
+  if (nodeExecPath) {
+    const splitPath = nodeExecPath.split("\\src\\");
+    const newPath = splitPath.reduce((a, b, idx) => {
+      if (idx === splitPath.length - 1) return path.join(a, "build", "src", b);
+      else return path.join(a, "src", b);
+    });
+    copyAndReplaceFile({
+      srcFile: nodeExecPath,
+      destFile: newPath,
+      file: "node.exe",
+    });
+  }
   copyAndReplaceFiles(publicFilesSrc, publicFilesDest);
 };
 if (require.main === module) {
--- packageScripts/utils/copyAndReplaceFiles..ts
+++ packageScripts/utils/copyAndReplaceFiles..ts
@@ -1,7 +1,26 @@
-import * as fs from 'fs';
-import * as path from 'path';
-import { copyFileSync, ensureDirSync } from 'fs-extra';
-
+import * as fs from "fs";
+import * as path from "path";
+import { copyFileSync, ensureDirSync } from "fs-extra";
+export function copyAndReplaceFile({
+  srcFile,
+  destFile,
+  file,
+}: {
+  srcFile: string;
+  destFile: string;
+  file: string;
+}) {
+  // If the file already exists in the destination directory, remove it
+  if (fs.existsSync(destFile)) {
+    fs.unlinkSync(destFile);
+  }
+  // Skip files with .ts extension
+  if (path.extname(file) === ".ts") {
+    return;
+  }
+  // Copy the file to the destination directory
+  copyFileSync(srcFile, destFile);
+}
 export function copyAndReplaceFiles(srcDir: string, destDir: string) {
   // Create destination directory if it doesn't exist
   ensureDirSync(destDir);
@@ -10,19 +29,9 @@ export function copyAndReplaceFiles(srcDir: string, destDir: string) {
   const files = fs.readdirSync(srcDir);
 
   // Copy and replace each file in the destination directory
-  files.forEach(file => {
+  files.forEach((file) => {
     const srcFile = path.join(srcDir, file);
     const destFile = path.join(destDir, file);
-
-    // If the file already exists in the destination directory, remove it
-    if (fs.existsSync(destFile)) {
-      fs.unlinkSync(destFile);
-    }
-    // Skip files with .ts extension
-    if (path.extname(file) === ".ts") {
-      return;
-    }
-    // Copy the file to the destination directory
-    copyFileSync(srcFile, destFile);
+    copyAndReplaceFile({ srcFile, destFile, file });
   });
 }
